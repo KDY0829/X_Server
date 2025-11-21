9c5fe58dd9b23739a7e8b54e92d8ccb3bb07518f
KDY0829 <idy970829@gmail.com>
2025-11-21 14:34:27 +0900

    validator 적용

--- router/posts.mjs
+++ router/posts.mjs
@@ -1,8 +1,14 @@
 // 글, 리스트 관련
 import express from "express";
 import * as postController from "../controller/post.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 
 const router = express.Router();
+const validatePost = [
+  body("text").trim().isLength({ min: 4 }).withMessage("최소 4자 이상 입력"),
+  validate,
+];
 
 // 전체 포스트 가져오기
 // 특정 id에 대한 포스트 가져오기
@@ -16,11 +22,11 @@ router.get("/:id", postController.getPost);
 
 // 포스트 쓰기
 // http://127.0.0.1:8000/post/
-router.post("/", postController.createPost);
+router.post("/", validatePost, postController.createPost);
 
 // 포스트 수정하기
 // http://127.0.0.1:8000/post/:id
-router.put("/:id", postController.updatePost);
+router.put("/:id", validatePost, postController.updatePost);
 
 // 포스트 삭제하기
 // http://127.0.0.1:8000/post/:id
