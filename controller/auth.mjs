c939f6475954dd994d5e3c0a930f7938291fcb23
KDY0829 <idy970829@gmail.com>
2025-11-24 15:23:18 +0900

    mySQL 적용 완료

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -4,15 +4,15 @@ import * as bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { config } from "../config.mjs";
 
-async function createJwtToken(id) {
-  return jwt.sign({ id }, config.jwt.secertKey, {
+async function createJwtToken(idx) {
+  return jwt.sign({ idx }, config.jwt.secertKey, {
     expiresIn: config.jwt.expiresInSec,
   });
 }
 
 // 회원가입 함수
 export async function signup(req, res, next) {
-  const { userid, password, name, email } = req.body;
+  const { userid, password, name, email, url } = req.body;
 
   // 회원 중복 체크
   const found = await authRepository.findByUserid(userid);
@@ -21,7 +21,13 @@ export async function signup(req, res, next) {
   }
 
   const hashed = bcrypt.hashSync(password, config.bcrypt.saltRounds);
-  const user = await authRepository.createUser(userid, hashed, name, email);
+  const user = await authRepository.createUser({
+    userid,
+    password: hashed,
+    name,
+    email,
+    url,
+  });
 
   //   const user = await authRepository.createUser(userid, password, name, email);
   const token = await createJwtToken(user.id);
@@ -43,15 +49,14 @@ export async function login(req, res, next) {
     return res.status(401).json({ message: `아이디 또는 비밀번호 확인` });
   }
 
-  const token = await createJwtToken(user.id);
+  const token = await createJwtToken(user.idx);
   res.status(200).json({ token, user });
 }
 
 export async function me(req, res, next) {
-  //   const user = await authRepository.findByUserid(req.id);
-  //   if (!user) {
-  //     return res.stutus(404).json({ message: "일치하는 사용자가 없음" });
-  //   }
-  //   res.status(200).json({ token: req.token, userid: user.userid });
-  res.status(200).json("성공");
+  const user = await authRepository.findById(req.idx);
+  if (!user) {
+    return res.stutus(404).json({ message: "일치하는 사용자가 없음" });
+  }
+  res.status(200).json({ token: req.token, userid: user.idx });
 }
--- router/posts.mjs
+++ router/posts.mjs
@@ -3,6 +3,7 @@ import express from "express";
 import * as postController from "../controller/post.mjs";
 import { body } from "express-validator";
 import { validate } from "../middleware/validator.mjs";
+import { isAuth } from "../middleware/auth.mjs";
 
 const router = express.Router();
 const validatePost = [
@@ -14,22 +15,22 @@ const validatePost = [
 // 특정 id에 대한 포스트 가져오기
 // http://127.0.0.1:8000/post
 // http://127.0.0.1:8000/post?userid=XXX
-router.get("/", postController.getPosts);
+router.get("/", isAuth, postController.getPosts);
 
 // 글 번호에 대한 포스트 가져오기
 // http://127.0.0.1:8000/post/:id
-router.get("/:id", postController.getPost);
+router.get("/:id", isAuth, postController.getPost);
 
 // 포스트 쓰기
 // http://127.0.0.1:8000/post/
-router.post("/", validatePost, postController.createPost);
+router.post("/", isAuth, validatePost, postController.createPost);
 
 // 포스트 수정하기
 // http://127.0.0.1:8000/post/:id
-router.put("/:id", validatePost, postController.updatePost);
+router.put("/:id", isAuth, validatePost, postController.updatePost);
 
 // 포스트 삭제하기
 // http://127.0.0.1:8000/post/:id
-router.delete("/:id", postController.deletePost);
+router.delete("/:id", isAuth, postController.deletePost);
 
 export default router;
