ad17ef1eb0a6fbc51295f073119241b1982acb52
KDY0829 <idy970829@gmail.com>
2025-11-26 11:32:02 +0900

    mongoose 적용 완료

--- data/auth.mjs
+++ data/auth.mjs
@@ -1,27 +1,33 @@
 import MongoDB from "mongodb";
-import { getUsers } from "../db/database.mjs";
+import { useVirtualId } from "../db/database.mjs";
+import mongoose from "mongoose";
 
-const ObjectID = MongoDB.ObjectId;
+// versoinKey: Mongoose가 문서를 저장할 때 자동으로 저장하는 _v라는 필드를 설정
+const userSchema = new mongoose.Schema(
+  {
+    userid: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    url: String,
+  },
+  { versionKey: false }
+);
+
+useVirtualId(userSchema);
+
+const User = mongoose.model("User", userSchema);
 
 // 회원가입
 export async function createUser(user) {
-  return getUsers()
-    .insertOne(user)
-    .then((result) => result.insertedId.toString());
+  return new User(user).save().then((data) => data.id);
 }
 
 // 로그인
 export async function findByUserid(userid) {
-  return getUsers().find({ userid }).next().then(mapOptionalUser);
+  return User.findOne({ userid });
 }
 
 export async function findById(id) {
-  return getUsers()
-    .find({ _id: new ObjectID(id) })
-    .next()
-    .then(mapOptionalUser);
-}
-
-function mapOptionalUser(user) {
-  return user ? { ...user, id: user._id.toString() } : user;
+  return User.findById(id);
 }
